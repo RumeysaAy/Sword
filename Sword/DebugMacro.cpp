30917c8878b0c1f50037d0dc1c36dc4155ff774f
RumeysaAy <rumeeysaay@gmail.com>
2024-12-01 15:47:05 +0300

    “part1”

---
+++ Sword/DebugMacro.cpp
@@ -0,0 +1,1 @@
+#include "DebugMacro.h"
--- Sword/Private/Items/Item.cpp
+++ Sword/Private/Items/Item.cpp
@@ -2,27 +2,19 @@
 
 
 #include "Items/Item.h"
+#include "Sword/DebugMacro.h"
 
 // Sets default values
 AItem::AItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play: ITU!"));
-
-	// ekrana yazı yazdırmak
-	if(GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("BeginPlay: Istanbul Technical University"));
-	}
 }
 
 // Called every frame
@@ -30,13 +22,13 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
-
-	// ekrana yazı yazdırmak
-	if(GEngine)
-	{
-		FString Message = FString::Printf(TEXT("DeltaTime: %f"), DeltaTime);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-	}
+	// Movement rate in units of cm/s
+	float MovementRate = 50.f;
+	float RotationRate = 45.f;
+	// MovementRate * DeltaTime = (cm/s) * (s/frame) = cm/frame
+	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
+	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+	DRAW_SPHERE_SingleFrame(GetActorLocation());
+	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 }
 
